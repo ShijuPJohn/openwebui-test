798e552c24ae33dd04efafe74b6672049e851fe1
ShijuPJohn <shijupjohn4u@gmail.com>
2025-06-07 14:55:19 +0530

    a dummy comment

--- backend/start.sh
+++ backend/start.sh
@@ -2,7 +2,7 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd "$SCRIPT_DIR" || exit
-
+#Just a dummy comment
 # Add conditional Playwright browser installation
 if [[ "${WEB_LOADER_ENGINE,,}" == "playwright" ]]; then
     if [[ -z "${PLAYWRIGHT_WS_URL}" ]]; then
